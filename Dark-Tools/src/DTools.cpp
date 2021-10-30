35f7fb0bc3ef0fa8683588d45ddf6a6d4ba121b2
YxY <876285983@qq.com>
2021-10-30 13:41:02 +0800

    feat: add macro and log tools

---
+++ Dark-Tools/src/DTools.cpp
@@ -0,0 +1,17 @@
+﻿/*************************************************
+Copyright © 2020 - 2021 YX.All Rights Reserved
+
+Date:2021-10-30 13:37
+Description:Dark Tools
+**************************************************/
+
+#include "DTools.h"
+
+namespace DTools {
+
+  void DKTools::Init()
+  {
+	Log::Init();
+  }
+
+}
---
+++ Dark-Tools/src/DTools.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Core/Macro.h"
+#include "Core/Log.h"
+
+namespace DTools {
+
+  class DKTools
+  {
+  public:
+	static void Init();
+  };
+
+}
